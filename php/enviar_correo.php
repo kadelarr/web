5a96164de80d2226aefddbeb263b0b527cb13fc4
Carlos Arley Delgado <carlos.delgado7@gmail.com>
2016-01-28 12:12:11 -0500

    final web

--- php/enviar_correo.php
+++ php/enviar_correo.php
@@ -1,12 +1,14 @@
 <?php
 
-$mail="kadelarr@hotmail.com";
+$mail="servicioalcliente@ingenieriaycampo.com";
 $nombre =$_POST['userName'];
 $email =$_POST['userEmail'];
+$telefono =$_POST['userTel'];
 $comentario =$_POST['userMsg'];
 $message="
 Nombre: ".$nombre."
 Email: ".$email."
+Telefono: ".$telefono."
 Comentario:".$comentario."
 ";
 if(mail ($mail,"Nuevo Mensaje", $message))
